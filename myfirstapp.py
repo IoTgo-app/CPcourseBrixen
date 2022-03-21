5f57434e93b7c69c99ca51fac6141a6066e8b642
rizMehdi <ccmeall@gmail.com>
2022-03-21 16:59:10 +0100

    Update myfirstapp.py

--- myfirstapp.py
+++ myfirstapp.py
@@ -1,8 +1,29 @@
 import streamlit as st
 
+# ! python3
+import json, requests 
 
-st.write('The is written with', 'st.write', '()')
+# add your own APIkey
+APIkey = '51f0821c87571383249cff5f9a44fe1d'
+location = 'london'
 
-st.text('The is written with' +  'st.text'+ '()')
+# check API documentation to see what structure of URL is needed to access the data
+# http://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+url = 'http://api.openweathermap.org/data/2.5/weather?q=' + location + '&appid=' + APIkey + '&units=metric'
+# print(url)
 
 
+# Download the JSON data from OpenWeatherMap.org's API.
+response = requests.get(url)  
+# Uncomment to see the raw JSON text:
+# print(response.text)  
+
+
+# Load JSON data into a Python variable.
+weatherData = json.loads(response.text)
+# Uncomment to see the raw JSON text:
+# print(weatherData) 
+# from pprint import pprint 
+# pprint(weatherData) 
+
+st.write((weatherData['main']['temp_max']))
