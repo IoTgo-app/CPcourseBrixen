87b05850ad45f2788d2fba943c5d59a7e41c3a87
rizMehdi <ccmeall@gmail.com>
2022-04-04 10:36:04 +0200

    Create translatorapp.py

---
+++ translatorapp.py
@@ -0,0 +1,15 @@
+import json, requests 
+from googletrans import Translator
+import streamlit as st
+
+translator = Translator()
+
+while True:
+  keyword= st.text_input('hi i am your translation chatbot. what you want to translate?', '')
+  keyword_it = translator.translate(keyword, src='en', dest='it')
+  if keyword == 'nothing':
+    print('ok. bye !')
+    break
+  st.write(keyword, 'is called', keyword_it.text, 'in italian')
+ 
+
