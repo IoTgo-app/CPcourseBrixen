727703f9929121afa831e32b6831bcc02e1c5233
rizMehdi <ccmeall@gmail.com>
2022-04-04 10:43:31 +0200

    Update translatorapp.py

--- translatorapp.py
+++ translatorapp.py
@@ -6,10 +6,11 @@ translator = Translator()
 
 while True:
   keyword= st.text_input('hi i am your translation chatbot. what you want to translate?', '')
-  keyword_it = translator.translate(keyword, src='en', dest='it')
-  if keyword == 'nothing':
-    print('ok. bye !')
-    break
-  st.write(keyword, 'is called', keyword_it.text, 'in italian')
+  if keyword!='':
+    keyword_it = translator.translate(keyword, src='en', dest='it')
+    if keyword == 'nothing':
+      print('ok. bye !')
+      break
+    st.write(keyword, 'is called', keyword_it.text, 'in italian')
  
 
