93438649909b9261f75339291a968be0642db071
rizMehdi <ccmeall@gmail.com>
2022-04-25 18:06:57 +0200

    Update myspeechapp.py

--- class06/myspeechapp.py
+++ class06/myspeechapp.py
@@ -10,8 +10,9 @@ tts1=gTTS(text='hello world')
 tts1.save("audio.mp3")
 
 audio_file = open("audio.mp3", "rb")
-audio_bytes = audio_file.read()
+audio_data = audio_file.read()
+
 st.markdown(f"## Your audio:")
-st.audio(audio_bytes, format="audio/mp3", start_time=0)
+st.audio(audio_file, format="audio/mp3", start_time=0)
 
 st.download_button(label="Download audio file", data=audio_file, file_name='youraudio.mp3',mime='audio/mp3')
