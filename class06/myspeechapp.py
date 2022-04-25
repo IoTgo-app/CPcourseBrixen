5b401e90fc39f907c78e2050c49b0a19735fc62c
rizMehdi <ccmeall@gmail.com>
2022-04-25 17:13:37 +0200

    Update myspeechapp.py

--- class06/myspeechapp.py
+++ class06/myspeechapp.py
@@ -12,9 +12,9 @@ except:
 st.write("hello")
 
 tts1=gTTS(text='hello world')
-tts1.save(f"temp/audio.mp3")
+tts1.save(f"audio.mp3")
 
-audio_file = open(f"temp/audio.mp3", "rb")
+audio_file = open(f"audio.mp3", "rb")
 audio_bytes = audio_file.read()
 st.markdown(f"## Your audio:")
 st.audio(audio_bytes, format="audio/mp3", start_time=0)
