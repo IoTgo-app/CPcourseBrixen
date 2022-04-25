df1fc5a9b4393a08c5c594c37d2a6e4595b92ad3
rizMehdi <ccmeall@gmail.com>
2022-04-25 19:43:47 +0200

    Update myspeechapp.py

--- class06/myspeechapp.py
+++ class06/myspeechapp.py
@@ -1,18 +1,1 @@
 #! python3
-from googletrans import Translator
-from gtts import gTTS 
-import streamlit as st
-  
-
-st.write("hello")
-
-tts1=gTTS(text='hello world')
-tts1.save("audio.mp3")
-
-audio_file = open("audio.mp3", "rb")
-audio_data = audio_file.read()
-
-st.markdown(f"## Your audio:")
-st.audio(audio_file, format="audio/mp3", start_time=0)
-
-st.download_button(label="Download audio file", data=audio_file, file_name='youraudio.mp3',mime='audio/mp3')
