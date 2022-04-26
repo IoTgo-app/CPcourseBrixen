74fc3a31ed9b4ea9f6c62ba1108d3b9de5a13697
rizMehdi <ccmeall@gmail.com>
2022-04-26 12:39:56 +0200

    Update myawesomecode.py

--- audio/myawesomecode.py
+++ audio/myawesomecode.py
@@ -1,1 +1,13 @@
+#! python3
+from googletrans import Translator
+from gtts import gTTS 
+import streamlit as st
 
+tts1=gTTS(text='hello world')
+tts1.save("audio.mp3")
+
+audio_file = open("audio.mp3", "rb")
+st.write("You can listen to the audio here:")
+st.audio(audio_file, format="audio/mp3", start_time=0)
+
+st.download_button(label="Download audio file", data=audio_file, file_name='youraudio.mp3',mime='audio/mp3')
