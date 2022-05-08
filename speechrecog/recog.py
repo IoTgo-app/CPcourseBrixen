4236425c414741199fe8e978d13222e99f43086e
rizMehdi <ccmeall@gmail.com>
2022-05-08 22:49:34 +0200

    Update recog.py

--- speechrecog/recog.py
+++ speechrecog/recog.py
@@ -1,6 +1,7 @@
 
 import speech_recognition as sr
 import streamlit as st
+import urllib2  # the lib that handles the url stuff
 
 r = sr.Recognizer()
 
@@ -8,15 +9,16 @@ r = sr.Recognizer()
 #AUDIO_FILE = "sample_audio_long.wav"    #these files are in files-section of Class06 folder, on MS Teams.
 #https://github.com/rizMehdi/CPcourseBrixen/blob/main/speechrecog/sample_audio_long.wav
 
-AUDIO_FILE="https://raw.githubusercontent.com/rizMehdi/CPcourseBrixen/main/speechrecog/sample_audio_long.wav"
+AUDIO_FILEurl="https://raw.githubusercontent.com/rizMehdi/CPcourseBrixen/main/speechrecog/sample_audio_long.wav"
+AUDIO_FILEdata = urllib2.urlopen(AUDIO_FILEurl)
 
 #st.file_uploader("upload your audio file", type="audio/wav")
 
 
-st.audio(AUDIO_FILE, format="audio/wav")
+st.audio(AUDIO_FILEdata, format="audio/wav")
 
 
-with sr.AudioFile(AUDIO_FILE.getvalue()) as source:
+with sr.AudioFile(AUDIO_FILEdata) as source:
     audio = r.record(source)  # read the entire audio file
 
 recognised_text= r.recognize_google(audio)
