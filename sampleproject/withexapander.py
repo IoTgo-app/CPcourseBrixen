5b4bcb66fcaa484d7dbc964565a2fcd7dd403b98
rizMehdi <ccmeall@gmail.com>
2022-05-25 16:07:32 +0200

    Update withexapander.py

--- sampleproject/withexapander.py
+++ sampleproject/withexapander.py
@@ -8,9 +8,15 @@ st.image(wikiimage, width=100)
 
 st.header("Audio-Wikipedia")
 
-with st.expander("About this app"):
-     st.write("This app converts an wikipedia article to audio file so that you can can listen to it on the go.")
-     st.write("A project by BlahBlah Blah, for the course of Computer Programing, MS Applied Linguistics, at unibz, Brixen, Italy")
+st.write("This app converts an wikipedia article to audio file so that you can can listen to it on the go.")
+st.write("A project by BlahBlah Blah, for the course of Computer Programing, MS Applied Linguistics, at unibz, Brixen, Italy")
+
+with st.expander("How to use):
+     st.write("""
+     - Just paste the URL of the wikipedia article, 
+     - select origin and target langauge 
+     - and listen to the audio
+     """) 
 
 
 st.text_input("Please paste here the URL of the wikipedia page you want to convert to audio")
@@ -19,8 +25,6 @@ st.selectbox("Please select the origin langauge of the wikipedia page",( "Englis
 st.selectbox("Please select the target langauge in which you want to hear the article",( "Italian", "Arabic", "English", "French"))
 
 st.audio( "https://upload.wikimedia.org/wikipedia/commons/c/c4/Muriel-Nguyen-Xuan-Chopin-valse-opus64-1.ogg", format="audio/ogg")
-
-st.markdown("""---""")
 
 
 with st.expander("See credits"):
